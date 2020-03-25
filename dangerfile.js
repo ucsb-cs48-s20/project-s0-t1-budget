79c35b6cfeecba9e9d74b4a4d617124e672d8249
Bryan Terce <bryan.terce@gmail.com>
2020-03-25 12:31:23 -0700

    Print both dependencies and devDependencies

--- dangerfile.js
+++ dangerfile.js
@@ -1,13 +1,11 @@
-schedule(async () => {
-  const packageDiff = await danger.git.JSONDiffForFile("package.json");
-  message(JSON.stringify(packageDiff));
+function messageDependencyChanges(packageDiff) {
   const dependenciesDiff = packageDiff.dependencies;
 
   if (dependenciesDiff) {
     const addedDependencies = dependenciesDiff.added;
 
     if (addedDependencies.length > 0) {
-      let msg = `This PR add or changes ${addedDependencies.length} dependencies:`;
+      let msg = `This PR adds ${addedDependencies.length} dependencies:`;
 
       for (const dep of addedDependencies) {
         msg += `\n* \`${dep}\`: \`${dependenciesDiff.after[dep]}\``;
@@ -16,4 +14,29 @@ schedule(async () => {
       message(msg);
     }
   }
+}
+
+function messageDevDependencyChanges(packageDiff) {
+  const dependenciesDiff = packageDiff.devDependencies;
+
+  if (dependenciesDiff) {
+    const addedDependencies = dependenciesDiff.added;
+
+    if (addedDependencies.length > 0) {
+      let msg = `This PR adds ${addedDependencies.length} dev dependencies:`;
+
+      for (const dep of addedDependencies) {
+        msg += `\n* \`${dep}\`: \`${dependenciesDiff.after[dep]}\``;
+      }
+
+      message(msg);
+    }
+  }
+}
+
+schedule(async () => {
+  const packageDiff = await danger.git.JSONDiffForFile("package.json");
+
+  messageDependencyChanges(packageDiff);
+  messageDevDependencyChanges(packageDiff);
 });
