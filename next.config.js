f4f09c1fe2eaab300523f26caa72211a9c52c53c
Bryan Terce <bryan.terce@gmail.com>
2020-04-04 22:01:37 -0700

    bt - change where secrets come from???

--- next.config.js
+++ next.config.js
@@ -10,7 +10,10 @@ module.exports = {
       process.env.REDIRECT_URI || "http://localhost:3000/api/callback",
     POST_LOGOUT_REDIRECT_URI:
       process.env.POST_LOGOUT_REDIRECT_URI || "http://localhost:3000/",
-    SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
+    SESSION_COOKIE_SECRET:
+      // This default is an arbitrary value which doesn't matter for development. Don't worry about it.
+      process.env.SESSION_COOKIE_SECRET ||
+      "viloxyf_z2GW6K4CT-KQD_MoLEA2wqv5jWuq4Jd0P7ymgG5GJGMpvMneXZzhK3sL",
     SESSION_COOKIE_LIFETIME: 7200, // 2 hours
   },
 };
