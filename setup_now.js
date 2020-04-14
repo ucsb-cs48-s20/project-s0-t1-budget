fda89824296fea4a006a5490ec534ceccd4548be
Bryan Terce <bryan.terce@gmail.com>
2020-04-14 12:36:11 -0700

    bt - update now to use project env vars
    Additionally, configure Auth0 to mock on inital deploy

--- setup_now.js
+++ setup_now.js
@@ -1,25 +1,16 @@
-// Read secrets from .env file and add them to now app
-// Prepend secrets with a string that disambiguates different
-// applications.
-
 require("dotenv").config();
 const crypto = require("crypto");
 const { execSync } = require("child_process");
-const fs = require("fs");
 const inquirer = require("inquirer");
 
-function getAppName(github, app) {
-  return `cs48-${github}-${app}`.toLowerCase();
-}
-
-function addSecret(appName, key, value) {
+function addEnvVar(key, value) {
   try {
-    execSync(`npx now secrets add "${appName}-${key}" "${value}"`, {
-      stdio: "inherit",
+    execSync(`npx now env add "${key}" production`, {
+      input: value,
     });
   } catch {
     console.error(
-      `Could not add "${appName}-${key}". If it already exists, that's fine.`
+      `Could not add "${key}". If it already exists, that's fine. If you want to update an existing environment variable, run "npx now env rm ${key} production"`
     );
   }
 }
@@ -28,70 +19,29 @@ inquirer
   .prompt([
     {
       type: "input",
-      name: "github",
-      message: "What's your github username?",
+      name: "url",
+      message: "What is your app's production url?",
       validate: (value) => {
-        // from https://github.com/shinnn/github-username-regex
-        const valid = /^[a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38}$/i.test(value);
-
-        return valid || "You must enter a valid GitHub username!";
-      },
-    },
-    {
-      type: "input",
-      name: "app",
-      message: "What do you want to name your app?",
-      validate: (value, { github }) => {
-        const appName = getAppName(github, value);
-        const valid =
-          appName.length <= 52 &&
-          !/[^a-z0-9\-]/.test(value) &&
-          !/--/.test(value) &&
-          !/-$/.test(value);
+        const valid = /^https:\/\//.test(value);
 
         return (
           valid ||
-          "Invalid app name. The full app name must be no more than 52 characters and only contain alphanumeric characters and non-consecutive/non-trailing dashes."
+          "Invalid url. This should be the production url copied from running 'npx now'"
         );
       },
     },
   ])
-  .then(({ github, app }) => {
-    const appName = getAppName(github, app);
-    const cookieSecret = crypto.randomBytes(32).toString("hex");
-    // This generates a random value for SESSION_COOKIE_SECRET
-    // The importance of this is explained here:
-    //   https://martinfowler.com/articles/session-secret.html
+  .then(({ url }) => {
+    addEnvVar("AUTH0_DOMAIN", process.env.AUTH0_DOMAIN);
+    addEnvVar("AUTH0_CLIENT_ID", process.env.AUTH0_CLIENT_ID);
+    addEnvVar("AUTH0_CLIENT_SECRET", process.env.AUTH0_CLIENT_SECRET);
+    addEnvVar("REDIRECT_URI", `${url}/api/callback`);
+    addEnvVar("POST_LOGOUT_REDIRECT_URI", url);
+    addEnvVar("SESSION_COOKIE_SECRET", crypto.randomBytes(32).toString("hex"));
 
-    const secrets = [
-      ["AUTH0_DOMAIN", null, process.env.AUTH0_DOMAIN],
-      ["AUTH0_CLIENT_ID", null, process.env.AUTH0_CLIENT_ID],
-      ["AUTH0_CLIENT_SECRET", "client_secret", process.env.AUTH0_CLIENT_SECRET],
-      ["REDIRECT_URI", null, `https://${appName}.now.sh/api/callback`],
-      ["POST_LOGOUT_REDIRECT_URI", null, `https://${appName}.now.sh/`],
-      ["SESSION_COOKIE_SECRET", "cookie_secret", cookieSecret],
-    ];
+    console.log("Configured all environment variables. Redeploying...");
 
-    const json = {
-      name: appName,
-      build: {
-        env: {},
-      },
-    };
-
-    for (const [jsonKey, secretKeyName, value] of secrets) {
-      if (secretKeyName) {
-        json.build.env[jsonKey] = `@${appName}-${secretKeyName}`;
-        addSecret(appName, secretKeyName, value);
-      } else {
-        json.build.env[jsonKey] = value;
-      }
-    }
-
-    fs.writeFileSync("now.json", JSON.stringify(json, null, "\t"));
-
-    console.log(
-      "Created a now.json file, you're now ready to deploy by running:"
-    );
-    console.log("        npx now --prod --confirm");
+    execSync("npx now --prod", {
+      stdio: "inherit",
+    });
   });
