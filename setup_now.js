70bafea75ea362cce97f255b68ab356ed428e624
Bryan Terce <bryan.terce@gmail.com>
2020-04-15 14:43:19 -0700

    bt - fix url concatenation

--- setup_now.js
+++ setup_now.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const crypto = require("crypto");
 const { execSync } = require("child_process");
+const url = require("url");
 const inquirer = require("inquirer");
 
 function addEnvVar(key, value) {
@@ -19,7 +20,7 @@ inquirer
   .prompt([
     {
       type: "input",
-      name: "url",
+      name: "prodUrl",
       message: "What is your app's production url?",
       validate: (value) => {
         const valid = /^https:\/\//.test(value);
@@ -31,12 +32,12 @@ inquirer
       },
     },
   ])
-  .then(({ url }) => {
+  .then(({ prodUrl }) => {
     addEnvVar("AUTH0_DOMAIN", process.env.AUTH0_DOMAIN);
     addEnvVar("AUTH0_CLIENT_ID", process.env.AUTH0_CLIENT_ID);
     addEnvVar("AUTH0_CLIENT_SECRET", process.env.AUTH0_CLIENT_SECRET);
-    addEnvVar("REDIRECT_URI", `${url}/api/callback`);
-    addEnvVar("POST_LOGOUT_REDIRECT_URI", url);
+    addEnvVar("REDIRECT_URI", url.resolve(prodUrl, "/api/callback"));
+    addEnvVar("POST_LOGOUT_REDIRECT_URI", prodUrl);
     // This generates a random value for SESSION_COOKIE_SECRET
     // The importance of this is explained here:
     //   https://martinfowler.com/articles/session-secret.html
