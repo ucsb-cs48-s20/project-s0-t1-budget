1a6713c8b2629644c6503d4927159cce99f44b3c
preethamkesineni <preethamkesineni@umail.ucsb.edu>
2020-04-27 13:24:59 -0700

    Implemented dropdown added valid number alert

--- components/ChartFormComponent.js
+++ components/ChartFormComponent.js
@@ -3,18 +3,32 @@ import { render } from "react-dom";
 class ChartFormComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
+    this.state = { input: "", category: "Groceries" };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleChange(event) {
-    this.setState({ value: event.target.value });
+    if (event.target.name == "input") {
+      let numTest = event.target.value;
+
+      if (!Number(numTest) && !(numTest == "")) {
+        alert("Please Enter a Valid Number you entered " + numTest);
+        return;
+      }
+    }
+
+    this.setState({ [event.target.name]: event.target.value });
   }
 
   handleSubmit(event) {
-    alert("A value was submitted " + this.state.value);
+    alert(
+      "A value was submitted input: " +
+        this.state.input +
+        " category: " +
+        this.state.category
+    );
     event.preventDefault();
   }
 
@@ -24,11 +38,22 @@ class ChartFormComponent extends React.Component {
         <h4>Monthly Expense</h4>
         <form onSubmit={this.handleSubmit}>
           <label>
-            Enter Value of expense($)
+            Enter Value of expense($):
             <br />
+            <select
+              name="category"
+              value={this.state.category}
+              onChange={this.handleChange}
+            >
+              <option value="Groceries">Groceries</option> 
+              <option value="Utility">Utility</option> 
+              <option value="Decor">Decoration</option> 
+              <option value="Other">Other</option> 
+            </select>
             <input
+              name="input"
               type="text"
-              value={this.state.value}
+              value={this.state.input}
               onChange={this.handleChange}
             />
           </label>
