370e66adaba1bf2985e48f8ecc6523aa8c6fd91a
Tomas Vera <tomasvera@Tomass-MacBook-Pro.local>
2020-04-30 18:10:11 -0700

    initial setup for chart

--- components/ChartComponent.js
+++ components/ChartComponent.js
@@ -2,12 +2,19 @@ import React, { Component } from "react";
 import { Bar } from "react-chartjs-2";
 
 export default class ChartComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chartData: props.chartData,
+    };
+  }
+
   render() {
     return (
       <div>
         <h2>Bar Graph</h2>
         <Bar
-          data={this.props.graph}
+          data={this.state.chartData}
           width={100}
           height={50}
           options={{
--- pages/index.js
+++ pages/index.js
@@ -2,50 +2,51 @@ import Layout from "../components/Layout";
 import ChartComponent from "../components/ChartComponent";
 import ChartFormComponent from "../components/ChartFormComponent";
 import { optionalAuth } from "../utils/ssr";
+import { Component } from "react";
 
 export const getServerSideProps = optionalAuth;
 
-var inputA = [];
+class HomePage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      chartData: {},
+    };
+  }
 
-var data = {
-  labels: inputA,
-  datasets: [
-    {
-      label: "My First dataset",
-      backgroundColor: "rgba(255,99,132,0.2)",
-      borderColor: "rgba(255,99,132,1)",
-      borderWidth: 1,
-      hoverBackgroundColor: "rgba(255,99,132,0.4)",
-      hoverBorderColor: "rgba(255,99,132,1)",
-      data: [10, 59, -20, 81, 56, 55, 40],
-    },
-  ],
-};
+  componentWillMount() {
+    this.getChartData();
+  }
 
-function handleClick() {
-  inputA.push("Added");
-  console.log(inputA);
-}
+  getChartData() {
+    this.setState({
+      chartData: {
+        labels: ["Groceries", "Utility", "Decor", "Other"],
+        datasets: [
+          {
+            label: "Categories",
+            backgroundColor: "rgba(255,99,132,0.2)",
+            borderColor: "rgba(255,99,132,1)",
+            borderWidth: 1,
+            hoverBackgroundColor: "rgba(255,99,132,0.4)",
+            hoverBorderColor: "rgba(255,99,132,1)",
+            data: [10, 59, -20, 81, 56, 55, 40],
+          },
+        ],
+      },
+    });
+  }
 
-function HomePage(props) {
-  const user = props.user;
-
-  return (
-    <Layout user={user}>
-      {user ? (
-        <div>
-          You're logged in! Here's what the server knows about you:
-          <pre>{JSON.stringify(user, null, "\t")}</pre>
-        </div>
-      ) : (
+  render() {
+    return (
+      <Layout>
         <div>
           <ChartFormComponent></ChartFormComponent>
-          <button onClick={handleClick}>addData</button>
-          <ChartComponent graph={data} />
+          <ChartComponent chartData={this.state.chartData} />
         </div>
-      )}
-    </Layout>
-  );
+      </Layout>
+    );
+  }
 }
 
 export default HomePage;
