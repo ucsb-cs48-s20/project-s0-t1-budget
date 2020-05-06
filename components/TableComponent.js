380f14e5f59a9cc216f12017952ce1273b806831
Trung Bui <trungbui@ucsb.edu>
2020-05-05 18:13:05 -0700

    tb+checks

--- components/TableComponent.js
+++ components/TableComponent.js
@@ -21,7 +21,35 @@ export default class TableComponent extends React.Component {
   // this.props.category contains an array of labels
   // this.props.data contains an array of values
   static getDerivedStateFromProps(props, state) {
-    // for i in (this.props.category.length(),
+    //  var i =  0;
+    //  var putValue = [];
+    //  var stop = props.category.length;
+    // //  console.log(stop)
+    //   while(i < stop)
+    //   {
+    //       if (i == 0)
+    //       {
+    //         dict = {
+    //           category: 'Net Income',
+    //           price: props.price[0]
+    //         }
+    //       }
+    //       else
+    //       {
+    //         dict = {
+    //           category: props.category[i],
+    //           price: props.price[i]
+    //         }
+    //       }
+    //       console.log(props.category)
+    //       console.log(props.price)
+    //       putValue.push(dict);
+    //       i++;
+    //   }
+    //   i = 0;
+    //   return {
+    //     values: putValue
+    //   };
   }
 
   render() {
