c6e0954c8114e27300b6a78bf5cc39656a3a8744
Trung Bui <trungbui@ucsb.edu>
2020-05-14 23:26:56 -0700

    test

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 module.exports = {
   env: {
     AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+    MONGODB_URI: process.env.MONGODB_URI,
     AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
     AUTH0_SCOPE: "openid profile",
