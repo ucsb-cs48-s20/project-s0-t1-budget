d47fc3c6c9e959361ada16eb2c45ebde34292dee
Trung Bui <trungbui@ucsb.edu>
2020-05-19 09:56:19 -0700

    tb-finished adding all needed functionality

--- pages/api/userbudgets/[id].js
+++ pages/api/userbudgets/[id].js
@@ -1,6 +1,14 @@
 import nextConnect from "next-connect";
 import middleware from "../../../middleware/database";
 
+/* The purpose of this file is to modify data in the database
+http://localhost:3000/api/userbudgets/ENDPOINT
+All method here will search by id and the ENDPOINT is the id here
+DELETE method - delete will delete data in the database
+GET method - will return the cursor to the data
+PUT method - will modify the data
+*/
+
 const { ObjectId } = require("mongodb");
 const Archetype = require("archetype-js"); //Handler conversion to MongoDB ObjectIds
 const handler = nextConnect();
@@ -13,18 +21,20 @@ handler.get(async (req, res) => {
     method,
   } = req;
 
+  // TB- Finding an user using their id as the endpoint
   switch (method) {
     case "GET":
       try {
+        //Reference to the findOne method https://docs.mongodb.com/manual/reference/method/db.collection.findOne/#definition
         const budget = await req.db
           .collection("database")
-          .find({ _id: Archetype.to(id, ObjectId) }); //Finding the budget by the id specified at the end points
-        console.log(stringify(budget));
+          .findOne({ _id: Archetype.to(id, ObjectId) }); //Finding the budget by the id specified at the end points
+        // console.log(JSON.stringify(budget,null)); test to see if the output is returning on the console
         //What if the budget does not exist??
         if (!budget) {
           return res.status(400).json({ success: false });
         }
-        res.status(200).json({ success: true });
+        res.status(200).json({ success: true, data: budget });
         //, data: JSON.stringify(budget) });
       } catch (error) {
         console.log(error); // showing what the error is
@@ -43,12 +53,31 @@ handler.put(async (req, res) => {
     method,
   } = req;
 
+  //Modifying the data with the PUT method
   switch (method) {
     case "PUT":
       try {
-        const budget = await req.db
-          .collection("database")
-          .findOneAndUpdate({ _id: Archetype.to(id, ObjectId) }, req.body);
+        if (req.body == "") {
+          //Nothing is passed inside the request body, WHAT ARE YOU TRYING TO UPDATE??? NOTHING?
+          console.log(
+            "Body request is: " +
+              req.body +
+              " and yes it's empty, please pass in a valid JSON string"
+          );
+          //Don't know what to pass in? Check out https://docs.mongodb.com/manual/reference/method/db.collection.findAndModify/ to find apporpriate update
+          //parameter to pass into the data
+          return res.status(400).json({ success: false });
+        }
+        /* Format to pass into the request body function, $set, $inc all works
+        {
+          "$set": {"month" : 200}
+        } */
+        const budget = await req.db.collection("database").updateOne(
+          { _id: Archetype.to(id, ObjectId) },
+
+          req.body,
+          { upsert: true }
+        );
 
         //What if the budget does not exist??
         if (!budget) {
@@ -56,6 +85,7 @@ handler.put(async (req, res) => {
         }
         res.status(200).json({ success: true, data: budget });
       } catch (error) {
+        console.log(error);
         res.status(400).json({ success: false });
       }
       break;
