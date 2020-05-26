7790973f14953627e6812973444882c551d4de7a
eddythai <edwardthai@ucsb.edu>
2020-05-26 03:07:52 -0700

    et-implemented changes to pie charts and added colorblind functionality to them

--- components/PieChartIncomeComponent.js
+++ components/PieChartIncomeComponent.js
@@ -1,42 +1,44 @@
 import React, { Component } from "react";
 import { Pie } from "react-chartjs-2";
-import { Card } from "react-bootstrap";
+import { draw } from "patternomaly";
 
-var bkColor, hbkColor;
+var expColor = "#d60a0a";
+var incColor = "#489c07";
 export default class PieChartIncomeComponent extends Component {
   state = {
-    labels: ["Red", "Blue", "Yellow"],
-    datasets: [
-      {
-        data: [300, 50, 100],
-        backgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-        hoverBackgroundColor: ["#FF6384", "#36A2EB", "#FFCE56"],
-      },
-    ],
-    isActive: true,
+    ColorBlind: false,
   };
 
   static getDerivedStateFromProps(props, state) {
-    var expColor = "#FF6384";
-    if (props.data[1] > 0) {
-      bkColor = "rgba(0,255,0,0.7)";
-    } else {
-      bkColor = "rgba(255,99,132,0.7)";
-    }
     var IncomeData = props.data.slice(0, 2);
     var Expenses = IncomeData[0] - IncomeData[1];
-    IncomeData[0] = Expenses;
-    return {
-      labels: ["Expenses", "Net Income"],
-      datasets: [
-        {
-          data: IncomeData,
-          backgroundColor: [expColor, bkColor],
-          hoverBackgroundColor: [expColor, bkColor],
-        },
-      ],
-      isActive: props.isActive,
-    };
+    IncomeData[1] = Expenses;
+    if (state.ColorBlind == true) {
+      return {
+        labels: ["Income", "Expenses"],
+        datasets: [
+          {
+            data: IncomeData,
+            backgroundColor: [
+              draw("cross", incColor),
+              draw("circle", expColor),
+            ],
+            hoverBackgroundColor: [incColor, expColor],
+          },
+        ],
+      };
+    } else {
+      return {
+        labels: ["Income", "Expenses"],
+        datasets: [
+          {
+            data: IncomeData,
+            backgroundColor: [incColor, expColor],
+            hoverBackgroundColor: [incColor, expColor],
+          },
+        ],
+      };
+    }
   }
 
   render() {
